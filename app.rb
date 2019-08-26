e5598eb9566f3e46a7dddca12afbe4a88f0f497b
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-26 20:49:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ module FormsLab
   
   post "/pirates" do
      @pirates = Pirate.new(params[:pirate])
-    params[:pirates][:ships].each do |details|
+    params[:pirate][:ships].each do |details|
       Ship.new(details)
     end
     
